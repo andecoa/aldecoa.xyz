2709dfad63ce992bc668ead27eb50490eff86fd8
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-12 18:20:06 -0700

    chore: set up linting and typescript

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "prettier"],
+  rules: {
+    "prettier/prettier": ["error"],
+    "react/react-in-jsx-scope": "off", // next.js does not require `import React...`
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ], // allow JSX for TypeScript files
+    "react/jsx-props-no-spreading": 0, // allow prop-spreading
+    "jsx-a11y/anchor-is-valid": [
+      // to disable errors for <Link href="/path"><a>Path</a></Link>
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
