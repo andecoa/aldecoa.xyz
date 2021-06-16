cdf789646041f3a5e6296995f2c293b34a82f79a
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-15 19:58:40 -0700

    feat: close navbar when route changes

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,15 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "@components/Navbar/Header";
 import Menu from "@components/Navbar/Menu";
+import Router from "next/router";
 
 export default function index() {
   const [menu, setMenu] = useState<boolean>(false);
+
+  useEffect(() => {
+    Router.events.on("routeChangeStart", () => setMenu(false));
+  }, []);
+
   const toggleMenu = (): void => {
     setMenu((isOpen) => !isOpen);
   };
 
   return (
-    <div>
+    <div className="fixed">
       <Header menu={menu} toggleMenu={toggleMenu} />
       <Menu menu={menu} />
     </div>
