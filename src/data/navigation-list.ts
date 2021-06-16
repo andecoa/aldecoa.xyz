0ec3c95a2666ff1772be33d6cdc435dd128f8ca4
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-15 19:18:08 -0700

    feat: set initial menu items

---
+++ src/data/navigation-list.ts
@@ -0,0 +1,30 @@
+import type TNavigation from "@ts/TNavigation";
+
+const data: TNavigation[] = [
+  {
+    name: "About",
+    href: "/about",
+  },
+  {
+    name: "Tech Stack",
+    href: "/tech-stack",
+  },
+  {
+    name: "Mental Models",
+    href: "/mental-models",
+  },
+  {
+    name: "Networks and Marketplaces",
+    href: "/networks-and-marketplaces",
+  },
+  {
+    name: "Blog",
+    href: "/blog",
+  },
+  {
+    name: "Projects",
+    href: "/projects",
+  },
+];
+
+export default data;
