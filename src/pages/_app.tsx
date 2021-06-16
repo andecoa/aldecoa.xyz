97949a44be5e303476386e99cc7782aa8e1d6bc3
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-15 19:32:57 -0700

    feat: wrap pages in a ThemeProvider and a default layout

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import "../styles/globals.css";
+import Layout from "@components/Layout";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider attribute="class">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default MyApp;
