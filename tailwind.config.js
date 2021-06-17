7810c925be0a7261a92f72e2f81ab14636f773ba
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-16 20:14:44 -0700

    chore: ignore css purge when NODE_ENV is set to 'development'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+const toPurge =
+  process.env.NODE_ENV === "development"
+    ? []
+    : [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+      ];
+
 module.exports = {
-  mode: "jit",
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
+  jit: true,
+  purge: toPurge,
   darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Lato", ...defaultTheme.fontFamily.sans],
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
       },
     },
   },
