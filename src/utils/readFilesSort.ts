0731b3760bc816b5b19f6386c18d59dfc8e45e8b
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-16 20:30:12 -0700

    refactor: change file name to be more semantic to what the function does

--- src/utils/readFilesSort.ts
+++ src/utils/readFilesSort.ts
@@ -1,13 +1,13 @@
 import { readdirSync, statSync } from "fs";
 
+export type TReadFilesSort = { name: string; dateCreated: number }[];
+
 /**
  * Gets the file names inside of the supplied path
  * @param path The path we are reading from containing all the file names we need
  * @returns An array of file names
  */
-export default function readFilesSort(
-  path: string
-): { name: string; dateCreated: number }[] {
+export default function readFilesSort(path: string): TReadFilesSort {
   const files = readdirSync(path);
   return files
     .map((file) => ({
